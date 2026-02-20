b433df894c1f9af3608804eeb65e9e1e8950c7fc
Kabbo | Frontend Developer <160781484+siyam1100@users.noreply.github.com>
2026-02-20 22:28:10 +0600

    Create StableToken.sol

---
+++ StableToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StableToken is ERC20, Ownable {
+    constructor() ERC20("AlgoCash", "CASH") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
